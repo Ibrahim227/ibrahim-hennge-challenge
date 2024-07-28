35259233a3fe128344fbfbd53b176b2b3b15902f
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-28 19:01:07 +0100

    update main.py

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ def main(*args: int) -> int:
 t = main(1, 2, -3, 4, 50, 100)
 print(t)
 #
-dist = {'username': 'Ibrahim', 'last_name': 'Manzo', 'password': "2123334"}
-sep = ':'
-x = sep.join(dist)
-print(str(x))
+# dist s
 
 if __name__ == '__main__':
     main()
